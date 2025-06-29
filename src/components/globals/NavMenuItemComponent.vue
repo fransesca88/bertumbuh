8fb27fd93ae592857cfbd74c7e46033b7c25477f
Fransesca <fransesca88@protonmail.com>
2025-06-29 15:54:42 +0800

    add: basic/starter modules
    Add: Footer
    Add: Header and Navigation
    Add: TailwindCSS
    Remove: unused starter files

---
+++ src/components/globals/NavMenuItemComponent.vue
@@ -0,0 +1,26 @@
+<template>
+  <RouterLink
+    :to="props.link"
+    class="text-bertumbuh-black transition-all border-b-2 border-b-bertumbuh-green/0 ease-in-out duration-150 hover:border-b-bertumbuh-green"
+    exact-active-class="border-b-2 border-b-bertumbuh-green text-bertumbuh-green"
+  >
+    <slot>{{ props.label }}</slot>
+  </RouterLink>
+</template>
+
+<script setup lang="ts">
+import { RouterLink } from 'vue-router'
+
+const props = defineProps({
+  link: {
+    type: String,
+    default: '',
+  },
+  label: {
+    type: String,
+    default: '',
+  },
+})
+</script>
+
+<style scoped></style>
